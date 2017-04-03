4ec2a8b370894444a009b0fbafbc79b0642f48de
Pashka Bosoy <pashabos19@mail.ru>
2017-04-03 23:28:31 +0300

    home page

--- app/js/main.js
+++ app/js/main.js
@@ -19,15 +19,53 @@ $(function () {
         }
     }
 
+    $(".js-menu").on('click', function(e){
+        $(this).siblings('ul').slideToggle(300);
+        $(this).toggleClass("active");
+    });
+
 
 
     /*=================Plagins===============*/
+    var sliderNav = $('.slider-nav-el');
 
-    $('.owl-carousel').slick({
+    $('.slick-carousel').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        vertical: true
+        vertical: true,
+        infinite: true,
+        lazyLoad: true,
+        draggable: false,
+        easing: 'easeInOutBounce',
+        dots: true,
+        prevArrow: '<div class="nav-top"><span class="icon icon-nav-top"/></div>',
+        nextArrow: '<div class="nav-bot"><span class="icon icon-nav-bottom"/></div>',
+        appendArrows: sliderNav,
+        appendDots: sliderNav,
+        customPaging: function(slider, i) {
+            var thumb = $(slider.$slides[i]).data();
+            return '<a>'+'0'+(i+1)+'</a>';
+        },
+        responsive: [{
+
+            breakpoint: 480,
+            settings: {
+                vertical: false,
+                dots: false
+            }
+
+        }]
     });
+
+    //custom function showing current slide
+/*    var $status = $('.pagingInfo');
+    var $slickElement = $('.slider');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + '/' + slick.slideCount);
+    });*/
 
 /*    $('.owl-carousel').owlCarousel({
         items:1,
